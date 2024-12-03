8dedd6219f7b51041c3e97a2cd01a083283e72c6
Ran Shaked <ran.shaked@runi.ac.il>
2024-12-03 19:25:20 +0200

    hw4

--- Primes.java
+++ Primes.java
@@ -1,32 +1,29 @@
-
 public class Primes {
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
-        Boolean[]P = new Boolean[N+1];
+        Boolean[] P = new Boolean[N + 1];
         int counter = 0;
-        for (int i = 0; i <= N; i++) {
-            if (i > 1) {
-                P[i] = true; 
-            } else {
-                P[i] = false; 
-            }
+
+        for (int i = 0; i < P.length; i++) {
+            P[i] = i > 1;
         }
-        for (int f = 2; f * f <= N; f++) {
-            if (P[f]) { 
-                for (int k = f * f; k <= N; k += f) {
-                    P[k] = false;
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (P[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    P[j] = false;
                 }
             }
         }
-        for(int j = 2; j<=N; j++)
-        {
-            if(P[j])
-            {
+
+        for (int i = 2; i <= N; i++) {
+            if (P[i]) {
                 counter++;
-                System.out.println(j);
+                System.out.println(i);
             }
         }
-        double s = ((double) counter / (N)) * 100;
+
+        double s = ((double) counter / (double) N) * 100;
         System.out.printf("There are %d primes between 2 and %d (%d%% are primes)%n", counter, N, (int) s);
+        }
     }
-}
