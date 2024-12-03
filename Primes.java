1049f7f082955422d171a70ee1299a3d77cd8c40
Ran Shaked <ran.shaked@runi.ac.il>
2024-12-03 19:12:47 +0200

    hw4

--- Primes.java
+++ Primes.java
@@ -4,19 +4,17 @@ public class Primes {
         int N = Integer.parseInt(args[0]);
         Boolean[]P = new Boolean[N+1];
         int counter = 0;
-        for(int i = 0 ; i < P.length ; i++)
-        {
-            if(i>1)
-            {
-            P[i] = true;
-            }else{
-                P[i] = false;
+        for (int i = 0; i <= N; i++) {
+            if (i > 1) {
+                P[i] = true; 
+            } else {
+                P[i] = false; 
             }
-            for(int f = 2;f <= (int)Math.sqrt(P.length+1); f++)
-            {
-                if(i%f == 0 && i!=f)
-                {
-                    P[i] = false;
+        }
+        for (int f = 2; f * f <= N; f++) {
+            if (P[f]) { 
+                for (int k = f * f; k <= N; k += f) {
+                    P[k] = false;
                 }
             }
         }
