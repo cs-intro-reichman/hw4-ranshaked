6f77d7756bb4cc8ef8e0e5fa576db411d595b042
Ran Shaked <ran.shaked@runi.ac.il>
2024-12-03 19:36:31 +0200

    hw4

--- Primes.java
+++ Primes.java
@@ -1,34 +1,35 @@
+
 public class Primes {
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
-        Boolean[] P = new Boolean[N + 1];
+        Boolean[]P = new Boolean[N+1];
         int counter = 0;
-
-        for (int i = 0; i < P.length; i++) {
-            if (i > 1) {
-                P[i] = true;
-            } else {
+        for(int i = 0 ; i < P.length ; i++)
+        {
+            if(i>1)
+            {
+            P[i] = true;
+            }else{
                 P[i] = false;
             }
-        }
-
-        for (int i = 2; i <= Math.sqrt(N); i++) {
-            if (P[i]) {
-                for (int j = i * i; j <= N; j += i) {
-                    P[j] = false;
+            for(int f = 2;f <= (int)Math.sqrt(P.length+1); f++)
+            {
+                if(i%f == 0 && i!=f)
+                {
+                    P[i] = false;
                 }
             }
         }
-
         System.out.println("Prime numbers up to " + N + ":");
-        for (int j = 0; j < P.length; j++) {
-            if (P[j]) {
+        for(int j = 2; j<=N; j++)
+        {
+            if(P[j])
+            {
                 counter++;
                 System.out.println(j);
             }
         }
-
-        double percentage = ((double) counter / N) * 100;
-        System.out.printf("There are %d primes between 2 and %d (%.0f%% are primes)%n", counter, N, percentage);
+        double s =((double)counter/(double)N)*100;
+        System.out.println("There are " + counter + " primes between 2 and " + N + " (" + (int)s + "% are primes)");
     }
-    }
+}
